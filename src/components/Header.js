4e2158f89fd87374c6677c6d7fef7bdcb5cdbaa9
Ultra Zacky <ultrazacky@gmail.com>
2022-04-22 09:03:56 -0700

    add Header function component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <React.Fragment>
+      <header>
+        <h1>McDonnel's Family Brewery</h1>
+        <hr/>
+      </header>
+    </React.Fragment>
+  )
+}
