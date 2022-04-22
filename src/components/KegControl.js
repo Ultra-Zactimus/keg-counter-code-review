d6de40708e1ab69ae52223e5ecd846d2ae86786a
Ultra Zacky <ultrazacky@gmail.com>
2022-04-22 14:10:08 -0700

    refactor code to try and figure out why it wont build

--- src/components/KegControl.js
+++ src/components/KegControl.js
@@ -5,7 +5,7 @@ import KegDetail from './KegDetail';
 
 
 class KegControl extends React.Component {
-  constructor(props) {
+  constructor(props){
     super(props);
     this.state = {
       formVisibleOnPage: false,
@@ -15,55 +15,51 @@ class KegControl extends React.Component {
   }
 
   handleClick = () => {
-    if (this.state.selectedKeg != null) {
+    if(this.state.selectedKeg != null) {
       this.setState({
         formVisibleOnPage: false,
         selectedKeg: null
       });
-    } else {
+    }else{
       this.setState(prevState => ({
-        formVisibleOnPage: !prevState.formVisibleOnPage
-      }));
+        formVisibleOnPage: !prevState.formVisibleOnPage}));
     }
   }
 
   handleAddingNewKegToList = (newKeg) => {
     const newMainKegList = this.state.mainKegList.concat(newKeg);
-    this.setState({ mainKegList: newMainKegList, formVisibleOnPage: false });
+    this.setState({mainKegList: newMainKegList, formVisibleOnPage: false});
   }
-
   handleChangingSelectedKeg = (id) => {
     const selectedKeg = this.state.mainKegList.filter(keg => keg.id === id)[0];
-    this.setState({ selectedKeg: selectedKeg });
+    this.setState({selectedKeg: selectedKeg});
   }
-
-  handleDeletingKeg = (id) => {
+  handleDeletingKeg=(id) => {
     const newMainKegList = this.state.mainKegList.filter(keg => keg.id !== id);
-    this.setState({ mainKegList: newMainKegList, selectedKeg: null });
+    this.setState({
+      mainKegList: newMainKegList, 
+      selectedKeg: null
+    });
   }
 
-  render() {
-    let currentlyVisableState = null;
+  render(){
+    let currentlyVisibleState = null;
     let buttonText = null;
     if (this.state.selectedKeg != null) {
-      currentlyVisableState = <KegDetail
-        keg={this.state.selectedKeg} onClickingDelete={this.handleDeletingKeg}
-      />
+      currentlyVisibleState = <KegDetail keg = {this.state.selectedKeg} onClickingDelete = {this.handleDeletingKeg} />
       buttonText = "Return to the list of Kegs";
-    } else if (this.state.formVisibleOnPage) {
-      currentlyVisableState = <NewKegForm
-        onNewKegCreation={this.handleAddingNewKegToList}
-      />
+    } 
+    else if(this.state.formVisibleOnPage){
+      currentlyVisibleState = <NewKegForm onNewKegCreation = {this.handleAddingNewKegToList}/>
       buttonText = "Return to the list of Kegs";
     } else {
-      currentlyVisableState = <KegList
-        kegList={this.state.mainKegList} onKegSelection={this.handleChangingSelectedKeg}
-      />
+      currentlyVisibleState = <KegList kegList = {this.state.mainKegList} onKegSelection = {this.handleChangingSelectedKeg}/>
+      buttonText = "Add Keg";
     }
-    return (
+    return(
       <React.Fragment>
-        {currentlyVisableState}
-        <button onClick={this.handleClick}>{buttonText}</button>
+        {currentlyVisibleState}
+        <button onClick ={this.handleClick}>{buttonText}</button>
       </React.Fragment>
     );
   }
--- src/components/NewKegForm.js
+++ src/components/NewKegForm.js
@@ -2,50 +2,37 @@ import React from "react";
 import { v4 } from "uuid";
 import PropTypes from "prop-types";
 
-function NewKegForm(props) {
-  function handleNewKegFormSubmission(event) {
+
+function NewKegForm(props){
+  function handleNewKegFormSubmission(event){
     event.preventDefault();
-    props.onNewKegCreation(
-      {
-        name: event.target.name.value,
-        brand: event.target.brand.value,
-        price: event.target.price.value,
-        alcoholContent: event.target.alcoholContent.value,
-        id: v4()
-      }
-    );
+    props.onNewKegCreation({name: event.target.name.value, brand: event.target.brand.value, price: event.target.price.value, alcoholContent: event.target.alcoholContent.value, id: v4()});
   }
   return (
     <React.Fragment>
-      <form onSubmit = { handleNewKegFormSubmission }>
+      <form onSubmit ={handleNewKegFormSubmission}>
         <input
-          type = "text"
-          name = "name"
-          placeholder = "Beer Name"
-        />
+          type="text"
+          name="name"
+          placeholder ="Beer Name"/>
         <input
-          type = "text"
-          name = "brand"
-          placeholder = "Brand Name"
-        />
+          type="text"
+          name="brand"
+          placeholder ="Brand Name"/>
         <input
-          type = "text"
-          name = "price"
-          placeholder = "Pricing"
-        />
+          type="text"
+          name="price"
+          placeholder ="Pricing"/>
         <input
           type = "text"
           name = "alcoholContent"
-          placeholder = "Alcohol Content"
-        />
+          placeholder = "Alcohol Content"/>
         <button type = "submit">Add Beer</button>
       </form>
     </React.Fragment>
   );
 }
-
 NewKegForm.propTypes = {
   onNewKegCreation: PropTypes.func
 };
-
 export default NewKegForm;
